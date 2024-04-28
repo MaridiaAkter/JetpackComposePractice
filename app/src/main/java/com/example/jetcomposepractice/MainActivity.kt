446f60ea1b987d2e2422c01050020068ffab35a2
maridia <maridiaakter929@gmail.com>
2024-04-28 16:56:11 +0600

    Code updated

--- app/src/main/java/com/example/jetcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetcomposepractice/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.jetcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.jetcomposepractice.ui.theme.JetComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,42 +29,72 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+               /* var count by remember {
+                    mutableIntStateOf(0)
+                }
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Mardia")
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click me!")
+
+                    }
+
+                }*/
+                var name by remember{
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()){
+                                names = names + name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+                    LazyColumn {
+                        items(names) { currentName ->
+                            Text(
+                                text = currentName,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+
+                            )
+                            Divider()
+                        }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Column(
-        horizontalAlignment = Alignment.End,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-          text = "Hello $name!",
-          color = Color.Red,
-          fontSize = 30.sp
-        )
-        Text(
-          text = "Hello $name!",
-          color = Color.Red,
-          fontSize = 30.sp
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetComposePracticeTheme {
-        Greeting("Mardia")
     }
 }
